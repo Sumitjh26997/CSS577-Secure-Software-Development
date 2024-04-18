c14578cccc6bdcac54b0009f99250d3bc43f33ba
Sumitjh26997 <sumit.hotchandani@gmail.com>
2024-04-17 17:09:40 -0700

    add comments to decryptor and polish

--- encryption/decryptor.ts
+++ encryption/decryptor.ts
@@ -3,66 +3,84 @@ import * as fs from 'fs';
 import { EncryptedFile } from './Types';
 import { generateKeys } from './utils';
 
+/**
+ * function to decrypt given file - file must be in the format of an EncryptedFile
+ * @param filePath - path to the encrypted file to be decrypted
+ * @param password - password to decrypt the file with
+ */
 const decryptFile = (filePath: string, password: string): void => {
-		const fileData: EncryptedFile = JSON.parse(fs.readFileSync(filePath).toString());
-		const { metadata, hmac, iv, encryptedData } = fileData;
-	
-		const { Km, Ke, Kh } = generateKeys(password, Buffer.from(metadata.salt, 'hex'), metadata.iterations, metadata.hashingAlgorithm);
+	// read the encrypted file data
+	const fileData: EncryptedFile = JSON.parse(fs.readFileSync(filePath).toString());
+	// destructure the file data into metadata, hmac, iv and encryptedData
+	const { metadata, hmac, iv, encryptedData } = fileData;
 
-		const hmacCheck = crypto.createHmac(metadata.hashingAlgorithm, Kh);
-		// hmacCheck.update(Buffer.from(metadata.salt, 'hex')).update(metadata.iterations.toString()).update(metadata.hashingAlgorithm).update(metadata.encryptionAlgorithm);
-		hmacCheck.update(Buffer.from(iv, 'hex')).update(Buffer.from(encryptedData, 'hex'));
+	// generate master, encryption and hmac keys
+	const { Km, Ke, Kh } = generateKeys(password, Buffer.from(metadata.salt, 'hex'), metadata.iterations, metadata.hashingAlgorithm);
+
+	// create a HMAC of the salt, iterations, hashing algorithm, encryption algorithm, iv and cipher text
+	const hmacCheck = crypto.createHmac(metadata.hashingAlgorithm, Kh);
+	hmacCheck.update(metadata.salt).update(metadata.iterations.toString()).update(metadata.hashingAlgorithm).update(metadata.encryptionAlgorithm);
+	hmacCheck.update(Buffer.from(iv, 'hex')).update(Buffer.from(encryptedData, 'hex'));
 
 
-		if(crypto.timingSafeEqual(hmacCheck.digest(), Buffer.from(hmac, 'hex'))) {
-			console.log("HMAC check successful");
-		} else {
-			throw new Error("HMAC check failed, file may have been tampered with");
-		}
-
-		let decipher: crypto.Decipher;
-		if(metadata.encryptionAlgorithm === 'aes-128-cbc') { 
-			decipher = crypto.createDecipheriv(metadata.encryptionAlgorithm, Ke.slice(0, 16), Buffer.from(iv, 'hex')); 
-		} else if (metadata.encryptionAlgorithm === 'des-ede3-cbc') {
-			decipher = crypto.createDecipheriv(metadata.encryptionAlgorithm, Ke.slice(0, 24), Buffer.from(iv, 'hex'));
-		} else {
-			decipher = crypto.createDecipheriv(metadata.encryptionAlgorithm, Ke, Buffer.from(iv, 'hex'));
-		}
-
-		let decryptedData = decipher.update(Buffer.from(encryptedData, 'hex'));
-		decryptedData = Buffer.concat([decryptedData, decipher.final()]);
-		console.log(decryptedData.toString());
-
-		const fileName = filePath.substring(filePath.lastIndexOf('/')+1);
-
-		fs.writeFileSync(`./${fileName.replace('.enc.json', '')}`, decryptedData);
-
+	// check if the HMAC of the file data matches the HMAC in the file
+	// used timingSafeEqual to prevent timing attacks
+	if(crypto.timingSafeEqual(hmacCheck.digest(), Buffer.from(hmac, 'hex'))) {
+		console.log("HMAC check successful");
+	} else {
+		throw new Error("HMAC check failed, file may have been tampered with");
 	}
 
+	// create a decipher object based on the block size of the encryption algorithm used
+	let decipher: crypto.Decipher;
+	if(metadata.encryptionAlgorithm === 'aes-128-cbc') { 
+		decipher = crypto.createDecipheriv(metadata.encryptionAlgorithm, Ke.slice(0, 16), Buffer.from(iv, 'hex')); 
+	} else if (metadata.encryptionAlgorithm === 'des-ede3-cbc') {
+		decipher = crypto.createDecipheriv(metadata.encryptionAlgorithm, Ke.slice(0, 24), Buffer.from(iv, 'hex'));
+	} else {
+		decipher = crypto.createDecipheriv(metadata.encryptionAlgorithm, Ke, Buffer.from(iv, 'hex'));
+	}
 
-	const validateOptions = (options: Record<string, string>): void => {
-		const requiredOptions = ['password', 'filePath'];
-		
-		requiredOptions.forEach((option) => {
-			if (!options[option]) {
-				throw new Error(`Missing required option: ${option}`);
-			}
-		});
+	// decrypt the file data
+	let decryptedData = decipher.update(Buffer.from(encryptedData, 'hex'));
+	decryptedData = Buffer.concat([decryptedData, decipher.final()]);
+
+	// write the decrypted data to a file
+	const fileName = filePath.substring(filePath.lastIndexOf('/')+1);
+	fs.writeFileSync(`./${fileName.replace('.enc.json', '')}`, decryptedData);
+}
+
+/**
+ * function to validate if the parameters passed are valid and complete
+ * @param options - options object containing all the arguments required for encryption
+ */
+const validateOptions = (options: Record<string, string>): void => {
+	const requiredOptions = ['password', 'filePath'];
 	
-		if (!fs.existsSync(options.filePath)) {
-			throw new Error(`File does not exist: ${options.filePath}`);
+	requiredOptions.forEach((option) => {
+		if (!options[option]) {
+			throw new Error(`Missing required option: ${option}`);
 		}
+	});
+
+	if (!fs.existsSync(options.filePath)) {
+		throw new Error(`File does not exist: ${options.filePath}`);
 	}
-	
-	const parseArgs = (args: string[]): Record<string, string> => {
-		const options: Record<string, string> = {};
-		for (let i = 2; i < args.length; i += 2) {
-				const key = args[i].replace('--', '');
-				options[key] = args[i + 1];
-		}
-		validateOptions(options);
-		return options;
+}
+
+/**
+ * function to parse command line arguments
+ * @param args - command line arguments
+ */
+const parseArgs = (args: string[]): Record<string, string> => {
+	const options: Record<string, string> = {};
+	for (let i = 2; i < args.length; i += 2) {
+			const key = args[i].replace('--', '');
+			options[key] = args[i + 1];
 	}
+	validateOptions(options);
+	return options;
+}
 
 	// Parse command line arguments
 const args = parseArgs(process.argv);
